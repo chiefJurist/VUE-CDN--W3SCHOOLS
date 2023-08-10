666d0115b44bf670769911c9e9d31760517d966f
chiefJurist <anthonynnanna004@gmail.com>
2023-08-10 07:40:44 +0100

    Initial commit

---
+++ 5-Other Mouse Events/app2.js
@@ -0,0 +1,20 @@
+const app =  Vue.createApp({
+    data(){
+        return{
+            showBooks: true,
+            title: "The Final Empire",
+            author: "Brandon Sanderson",
+            age: 45
+        }
+    },
+    methods:{
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e){
+            console.log(e)
+        }
+    }
+})
+
+app.mount('#app')
---
+++ 5-Other Mouse Events/app4.js
@@ -0,0 +1,23 @@
+const app =  Vue.createApp({
+    data(){
+        return{
+            showBooks: true,
+            title: "The Final Empire",
+            author: "Brandon Sanderson",
+            age: 45
+        }
+    },
+    methods:{
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e,data){
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        }
+    }
+})
+
+app.mount('#app')
