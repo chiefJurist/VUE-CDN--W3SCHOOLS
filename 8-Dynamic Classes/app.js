cbbbc6a9de8adae1099581f2831f39097163e400
chiefJurist <anthonynnanna004@gmail.com>
2023-08-10 12:30:55 +0100

    Initial commit

---
+++ 8-Dynamic Classes/app.js
@@ -0,0 +1,20 @@
+const app =  Vue.createApp({
+    data(){
+        return{
+            showBooks: true,
+            url: "https://netninja.co.uk",
+            books: [
+                {title: 'Name of the wind', author: 'Patrick Rothfuss', img:'assets/img1.jpg', isFav: true},
+                {title: 'The Way of Kings', author: 'Brandon Sanderson', img:'assets/img2.jpg', isFav: false},
+                {title: 'The Final Empire', author: 'Brandon Sanderson', img:'assets/img3.jpg', isFav: true}
+            ]
+        }
+    },
+    methods:{
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+})
+
+app.mount('#app')
