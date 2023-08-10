6ef25380884fd6ea5e23a84acd5a594d5c6a5dfe
chiefJurist <anthonynnanna004@gmail.com>
2023-08-10 16:26:37 +0100

    Initial commit

---
+++ 10-Computed Properties/app.js
@@ -0,0 +1,28 @@
+const app =  Vue.createApp({
+    data(){
+        return{
+            showBooks: true,
+            books: [
+                {title: 'Name of the wind', author: 'Patrick Rothfuss', img:'assets/img1.jpg', isFav: true},
+                {title: 'The Way of Kings', author: 'Brandon Sanderson', img:'assets/img2.jpg', isFav: false},
+                {title: 'The Final Empire', author: 'Brandon Sanderson', img:'assets/img3.jpg', isFav: true}
+            ]
+        }
+    },
+    methods:{
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        toggleFav(item){
+            item.isFav = !item.isFav
+        }
+    },
+
+    computed:{
+        filteredBooks(){
+            return this.books.filter((book)=> book.isFav)
+        }
+    }
+})
+
+app.mount('#app')
