47bc01f41463fd3f0bc5c7e134f022e553256775
chiefJurist <anthonynnanna004@gmail.com>
2023-08-10 12:54:02 +0100

    Initial commit

--- 7-Attribute Binding/app2.js
+++ 7-Attribute Binding/app2.js
@@ -2,7 +2,6 @@ const app =  Vue.createApp({
     data(){
         return{
             showBooks: true,
-            url: "https://netninja.co.uk",
             books: [
                 {title: 'Name of the wind', author: 'Patrick Rothfuss', img:'assets/img1.jpg'},
                 {title: 'The Way of Kings', author: 'Brandon Sanderson', img:'assets/img2.jpg'},
